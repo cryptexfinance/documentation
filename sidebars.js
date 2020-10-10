1b5f9cc6789800b6f2906d1b1c4992f87dc2b401
Cristian Espinoza <ceegarner@gmail.com>
2020-10-09 18:30:17 -0600

    added more contracts

--- sidebars.js
+++ sidebars.js
@@ -2,6 +2,10 @@ module.exports = {
   someSidebar: {
     Introduction: ["intro1", "intro2", "intro3", "intro4"],
     "How To Use": ["intro1", "intro2", "intro3"],
-    Contracts: ["contracts/ivaulthandler"],
+    Contracts: [
+      "contracts/ivaulthandler",
+      "contracts/tcap",
+      "contracts/orchestrator",
+    ],
   },
 };
