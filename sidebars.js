d7766a067bd7bd8575a0aa78d08e710fb27f0f98
Cristian Espinoza <ceegarner@gmail.com>
2020-10-11 10:15:27 -0600

    updated contracts

--- sidebars.js
+++ sidebars.js
@@ -1,9 +1,11 @@
 module.exports = {
-  someSidebar: {
+  tcapSidebar: {
     Introduction: ["intro1", "intro2", "intro3", "intro4"],
     "How To Use": ["intro1", "intro2", "intro3"],
     Contracts: [
       "contracts/ivaulthandler",
+      "contracts/ethvaulthandler",
+      "contracts/erc20vaulthandler",
       "contracts/tcap",
       "contracts/orchestrator",
     ],
